bfb20ad8a8cf773c6147a02bf4e6d4b0fd22fb99
Maaaksl87 <maaaksl87@gmail.com>
2025-11-06 00:17:55 +0200

    conected <CssBaseline /> and created base Theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+// створюємо базову тему
+const theme = createTheme({
+  palette: {
+    // colors
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
