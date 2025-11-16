1c766f138ea69ddd71b148bf311f9a9c559dfe01
Maaaksl87 <maaaksl87@gmail.com>
2025-11-16 13:43:25 +0200

    Connected shadcn/ui and tailwind according to the documentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import TransactionsPage from "./pages/TransactionsPage";
+import { Button } from "@/components/ui/button"
+import "./index.css";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+      
+//         <Route element={<Layout />}>
+//           <Route path="/" element={<DashboardPage />} />
+//           <Route path="/transactions" element={<TransactionsPage />} />
+//         </Route>
+
+       
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Всі маршрути, які мають мати Sidebar,будуть обгорнуті в 'Layout' */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
-        </Route>
-
-        {/* сторінки без Layout */}
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        {/* <Route path="/register" element={<RegisterPage />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App;
