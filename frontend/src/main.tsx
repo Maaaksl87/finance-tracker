98677a4397a30fc202e3459ed6027d3960842320
Maaaksl87 <maaaksl87@gmail.com>
2026-01-29 19:13:25 +0200

    implement React Query for data fetching with caching and devtools integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import "./index.css"; 
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Скільки часу дані вважаються "свіжими" (не потребують оновлення)
+      staleTime: 1000 * 60 * 5, // 5хв
+      
+      // Скільки зберігати в кеші після того, як компонент відмонтувався
+      gcTime: 1000 * 60 * 10, // 10хв
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  }
+})
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
