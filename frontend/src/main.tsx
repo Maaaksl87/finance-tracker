855c80317fa204d4788ed6b04c9f7725ef272a32
Maaaksl87 <maaaksl87@gmail.com>
2026-03-12 00:28:33 +0200

    Add @vercel/speed-insights package and integrate into main component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,29 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
-import "./index.css"; 
+import App from './App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       // Скільки часу дані вважаються "свіжими" (не потребують оновлення)
       staleTime: 1000 * 60 * 5, // 5хв
-      
+
       // Скільки зберігати в кеші після того, як компонент відмонтувався
       gcTime: 1000 * 60 * 10, // 10хв
       retry: 1,
       refetchOnWindowFocus: false,
     },
-  }
-})
+  },
+});
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
