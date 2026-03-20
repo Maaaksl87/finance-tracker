51dbc3c4744f4d23603b9fb27acdf29acdd66eda
Maksym Lytvynenko <99558021+Maaaksl87@users.noreply.github.com>
2026-03-21 01:28:47 +0200

    Change SVGProps type from SVGElement to SVGSVGElement

--- frontend/src/assets/icons/AppleIcon.tsx
+++ frontend/src/assets/icons/AppleIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const AppleIcon: React.FC<React.SVGProps<SVGElement>> = () => (
+const AppleIcon: React.FC<React.SVGProps<SVGSVGElement>> = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <path
       fill="currentColor"
