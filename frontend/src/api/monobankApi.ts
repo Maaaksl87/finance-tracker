ad9c20d269ed712ff908afaf678e10788c94a2e2
Maaaksl87 <maaaksl87@gmail.com>
2026-04-18 16:12:34 +0300

    change url for production

--- frontend/src/api/monobankApi.ts
+++ frontend/src/api/monobankApi.ts
@@ -1,7 +1,8 @@
 import type { CurrencyRate } from "@/types";
 
 export const fetchCurrencies = async (): Promise<CurrencyRate[]> => {
-  const response = await fetch("http://localhost:3000/currencies");
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const response = await fetch(`${API_URL}/currencies`);
   if (!response.ok) {
     throw new Error(`Помилка запиту: ${response.status}`);
   }
